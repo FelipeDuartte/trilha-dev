93b2fa5d70e6e5723a525c8cdd29f066df3ad0e5
Felipe <felipeduarte8266@gmail.com>
2025-08-12 12:29:29 -0300

    organize

--- script_/main.js
+++ script_/main.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const element = document.getElementById("typing-title");
-  const fullText = "O caminho certo para você se tornar um desenvolvedor de sucesso.";
+  const fullText =
+    "O caminho certo para você se tornar um desenvolvedor de sucesso.";
   const highlight = "desenvolvedor de sucesso.";
   const typingSpeed = 60;
-  let i = 0
+  let i = 0;
   function typeWriter() {
     if (i < fullText.length) {
       const current = fullText.substring(0, i + 1);
@@ -20,33 +21,33 @@ document.addEventListener("DOMContentLoaded", () => {
   element.innerHTML = "";
   typeWriter();
 });
- function revealCardsOnScroll() {
-    const cards = document.querySelectorAll('.track-card');
-    const triggerBottom = window.innerHeight * 0.85;
+function revealCardsOnScroll() {
+  const cards = document.querySelectorAll(".track-card");
+  const triggerBottom = window.innerHeight * 0.85;
 
-    cards.forEach(card => {
-      const cardTop = card.getBoundingClientRect().top;
+  cards.forEach((card) => {
+    const cardTop = card.getBoundingClientRect().top;
 
-      if (cardTop < triggerBottom) {
-        card.classList.add('visible');
-      }
-    });
-  }
+    if (cardTop < triggerBottom) {
+      card.classList.add("visible");
+    }
+  });
+}
 
-  window.addEventListener('scroll', revealCardsOnScroll);
-  window.addEventListener('load', revealCardsOnScroll);
-  function revealOnScroll() {
-    const elements = document.querySelectorAll('.fade-up');
-    const triggerBottom = window.innerHeight * 0.9;
+window.addEventListener("scroll", revealCardsOnScroll);
+window.addEventListener("load", revealCardsOnScroll);
+function revealOnScroll() {
+  const elements = document.querySelectorAll(".fade-up");
+  const triggerBottom = window.innerHeight * 0.9;
 
-    elements.forEach(el => {
-      const boxTop = el.getBoundingClientRect().top;
+  elements.forEach((el) => {
+    const boxTop = el.getBoundingClientRect().top;
 
-      if (boxTop < triggerBottom) {
-        el.classList.add('visible');
-      }
-    });
-  }
+    if (boxTop < triggerBottom) {
+      el.classList.add("visible");
+    }
+  });
+}
 
-  window.addEventListener('scroll', revealOnScroll);
-  window.addEventListener('load', revealOnScroll);
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
--- script_/script.js
+++ script_/script.js
@@ -1,38 +1,37 @@
 // Botão Voltar ao Topo
-const backToTopButton = document.querySelector('.back-to-top');
-window.addEventListener('scroll', () => {
+const backToTopButton = document.querySelector(".back-to-top");
+window.addEventListener("scroll", () => {
   if (window.scrollY > 500) {
-    backToTopButton.classList.add('show');
+    backToTopButton.classList.add("show");
   } else {
-    backToTopButton.classList.remove('show');
+    backToTopButton.classList.remove("show");
   }
 });
-backToTopButton.addEventListener('click', () => {
+backToTopButton.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 });
 
 //links aparecendo e escondendo
- document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButtons = document.querySelectorAll(".toggle-resources");
 
-        const toggleButtons = document.querySelectorAll('.toggle-resources');
-        
-        toggleButtons.forEach(button => {
-            button.addEventListener('click', function() {
-                const links = this.nextElementSibling;
-                const icon = this.querySelector('i');
-                
-                if (links.style.display === 'none' || !links.style.display) {
-                    links.style.display = 'flex';
-                    icon.classList.remove('fa-chevron-down');
-                    icon.classList.add('fa-chevron-up');
-                } else {
-                    links.style.display = 'none';
-                    icon.classList.remove('fa-chevron-up');
-                    icon.classList.add('fa-chevron-down');
-                }
-            });
-        });
+  toggleButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const links = this.nextElementSibling;
+      const icon = this.querySelector("i");
+
+      if (links.style.display === "none" || !links.style.display) {
+        links.style.display = "flex";
+        icon.classList.remove("fa-chevron-down");
+        icon.classList.add("fa-chevron-up");
+      } else {
+        links.style.display = "none";
+        icon.classList.remove("fa-chevron-up");
+        icon.classList.add("fa-chevron-down");
+      }
     });
+  });
+});
